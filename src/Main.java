b408d569579fed7e6af061ac674e8b848b34baa6
aguandstrika <kingsleyyoung27@gmail.com>
2020-11-13 21:18:15 -0500

    ADDING

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Planing / mapping out the project
+ *
+ *main.java
+ * layout.class
+ *
+ *Customer.java
+ * v customerUserName
+ * v customerAddress
+ * v customerPhoneNumber
+ * v customerAccountNumber random generator
+ * customer.class ~constructor
+ *
+ *
+ *Car.java
+ * car.class ~constructor
+ * -models:
+ * --540 $27,700 base
+ * --560 $32,500
+ * --570 $36,000
+ * --580 $44,000
+ *
+ * -Package:
+ * --A $2,200 available for all models
+ * --B $3250 available only on 570 and 580
+ *
+ * -Color:
+ * --Metallic paint option for $650.00
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
