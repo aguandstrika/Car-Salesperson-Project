9428bbd43b2d546040bd704924ca89ecab1020af
aguandstrika <kingsleyyoung27@gmail.com>
2020-11-13 22:14:37 -0500

    Updating  Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,24 @@
+import java.util.Random;
 public class Customer {
+    String customerUserName;
+    String customerAddress;
+    int customerPhoneNumber;
+    int customerAccountNumber; //random generator
+
+    //making a new random
+    Random rand = new Random();
+
+    //
+    Customer(String customerUserName, String customerAddress, int customerPhoneNumber, int customerAccountNumber){
+        this.customerUserName = customerUserName;
+        this.customerAddress = customerAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+        this.customerAccountNumber = customerAccountNumber;
+    }
+
+    public int getCustomerAccountNumber() {
+        return customerAccountNumber;
+    } = rand.(1000);
+
+
 }
