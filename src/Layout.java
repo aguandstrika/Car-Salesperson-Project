6495b66b8b79ccdeff7086b2b73bc60de66e8ad1
aguandstrika <kingsleyyoung27@gmail.com>
2020-11-21 19:54:49 -0500

    gridelayout Layout.java

--- src/Layout.java
+++ src/Layout.java
@@ -11,6 +11,9 @@ public class Layout{
     JFrame frame;
     JPanel panel;
     JPanel aderessPanel;
+    JPanel panelTwo;
+    JPanel panelThree;
+    JPanel panelFour;
     JLayeredPane glassPanel;
     JPanel userInFoPanel;
     JButton button;
@@ -26,48 +29,42 @@ public class Layout{
 
     public void layout() {
 
-
+       /***********************Start of Panel one / user info****************************/
         //user ID
         userID = new JLabel("ID");
         userID.setOpaque(true);
         userID.setSize(210,50);
         userID.setText("random id number"); //I'm trying to figure out how to add the random number generator from customer.java
-        userID.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         //phone Number
         phoneNumberInput = new JTextField("Phone Number: ",10);
         phoneNumberInput.setOpaque(true);
         phoneNumberInput.setSize(200,50);
-        phoneNumberInput.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 
         //first and last name
-        firstNameInput = new JTextField("Fist Name: ", 10);
-        lastNameInput = new JTextField("Last Name: ", 10);
+        firstNameInput = new JTextField("Fist Name: ", 20);
+        lastNameInput = new JTextField("Last Name: ", 20);
         firstNameInput.setOpaque(true);
         lastNameInput.setOpaque(true);
-        firstNameInput.setSize(200,50);
-        lastNameInput.setSize(200,50);
-        firstNameInput.setAlignmentX(Component.LEFT_ALIGNMENT);
-        lastNameInput.setAlignmentX(Component.LEFT_ALIGNMENT);
+        firstNameInput.setSize(200,100);
+        lastNameInput.setSize(200,100);
+
+        firstNameInput.setHorizontalAlignment(SwingConstants.LEFT);
+
 
         adderessInput = new JTextField("Adderese:", 80);
         adderessInput.setOpaque(true);
-        adderessInput.setSize(300,50);
-        adderessInput.setAlignmentX(Component.LEFT_ALIGNMENT);
+        adderessInput.setSize(900,50);
 
         aderessPanel = new JPanel();
-        aderessPanel.setAlignmentY(Component.TOP_ALIGNMENT);
         aderessPanel.setMaximumSize(new Dimension(960, 60));
         aderessPanel.setBackground(Color.PINK);
         aderessPanel.add(adderessInput);
 
         // first panel user info
         userInFoPanel = new JPanel();
-        //userInFoPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        userInFoPanel.setAlignmentY(Component.TOP_ALIGNMENT);
-
-        userInFoPanel.setMaximumSize(new Dimension(960, 60));
+        userInFoPanel.setMaximumSize(new Dimension(960, 50));
         userInFoPanel.setBackground(Color.PINK);
         userInFoPanel.add(firstNameInput);
         userInFoPanel.add(lastNameInput);
@@ -75,21 +72,81 @@ public class Layout{
         userInFoPanel.add(userID);
         userInFoPanel.add(aderessPanel);
 
-        // master panel
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel,BoxLayout.LINE_AXIS));
-        panel.add(Box.createRigidArea(new Dimension(4,4)));
-        panel.add(userInFoPanel);
-        //panel.add(aderessPanel);
+
+        /**********************End of Panel/ portion ome**********************/
+
+        /****************************Start of panel two***********************************/
+
+        panelTwo = new JPanel();
+        panelTwo.setMaximumSize(new Dimension(960, 200));
+        panelTwo.setBounds(64,0,960,200);
+        panelTwo.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+        panelTwo.setAlignmentY(Component.CENTER_ALIGNMENT);
+        panelTwo.add(Box.createRigidArea(new Dimension(4,4)));
+        panelTwo.setBackground(Color.CYAN);
+
+        /****************************End of panel two****************************************************/
+
+
+
+
+        /****************************Start of panel three***********************************/
+
+        panelThree = new JPanel();
+        panelThree.setMaximumSize(new Dimension(960, 300));
+        panelThree.setBounds(64,0,960,200);
+        panelThree.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+        panelThree.setAlignmentY(Component.CENTER_ALIGNMENT);
+        panelThree.add(Box.createRigidArea(new Dimension(4,4)));
+        panelThree.setBackground(Color.orange);
+
+        /****************************End of panel three****************************************************/
+
+        /****************************Start of panel four***********************************/
+
+        panelFour = new JPanel();
+        panelFour.setMaximumSize(new Dimension(960, 300));
+        panelFour.setBounds(64,0,960,200);
+        panelFour.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+        panelFour.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+        panelFour.add(Box.createRigidArea(new Dimension(4,4)));
+        panelFour.setBackground(Color.magenta);
+
+        /****************************End of panel four****************************************************/
+
+
+//        // master panel
+//        panel = new JPanel();
+//        //panel.setLayout(new BoxLayout(panel,BoxLayout.LINE_AXIS));
+//        panel.setLayout(new GridLayout(6,1));
+//        panel.add(Box.createRigidArea(new Dimension(4,4)));
+//        panel.add(userInFoPanel);
+//        panel.add(panelTwo);
+//        panel.add(panelThree);
+//        panel.add(panelFour);
+
 
         //making the frame
         frame = new JFrame();
         frame.setTitle("Cars That Go");
         frame.setSize(960,840);
         frame.setResizable(true);
-        frame.setBackground(Color.ORANGE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
+        //frame.add(panel);
+        frame.add(userInFoPanel);
+        frame.add(panelTwo);
+        frame.add(panelThree);
+        frame.add(panelFour);
+        frame.setLayout(new GridLayout(6,1));
+
+
+
+
+
+
+
+
 
 
         //Setting frame to visible
@@ -104,15 +161,5 @@ public class Layout{
         Customer newCustomer = new Customer("bob", "bob street", 19992939);
         System.out.println(newCustomer);
     }
-
-
-
-//    public static void addCustomer(){
-//
-//        //making sample customer with random number generator
-//        Customer newCustomer = new Customer("bob", "bob street", 19992939);
-//        newCustomer.customerAccountNumber();
-//        System.out.println(newCustomer);
-//    }
 
 }
